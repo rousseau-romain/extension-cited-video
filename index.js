6afa55e7227e3cc1d1c1372e0e949de7d17bd84a
rousseau-romain <rousseau.romain82@gmail.com>
2021-11-21 05:29:35 +0100

    add video show

--- index.js
+++ index.js
@@ -1,19 +1,129 @@
+const youtubeEmbedUrl = 'https://www.youtube.com/embed'
+
+const testUrl = 'https://www.youtube.com/watch?v=aHy-Y007JJ0'
+
+const timeoutLoadVideo = 2
+
 const splitInfo = (x, dateDefault) => {
   const strSplit    = x.split(' - ')
   const arrayMinSec = strSplit[0].split(':')
   const miliseconds = (arrayMinSec[0] / 60 + arrayMinSec[1]) * 1000
-  console.log(miliseconds / 1000)
   return {
-    date: new Date(dateDefault.getTime() + miliseconds),
-    url:  strSplit[1],
+    date   : new Date(dateDefault.getTime() + miliseconds),
+    url    : strSplit[1],
+    idVideo: (new URL(testUrl)).searchParams.get('v'),
   }
 }
 
-const formatTimeUniqueSeconds = (date) => `${date.getHours()},${date.getSeconds()},${date.getMinutes()}`
+const formatTimeUniqueSeconds = (date) => `${date.getHours()},${date.getMinutes()},${date.getSeconds()}`
+
+const createElement = (type, parameters) => {
+  const element = document.createElement(type)
+
+  const parametersDefault = ['id', 'textContent']
+
+  Object.keys(parameters).forEach((parameter) => {
+    // eslint-disable-next-line no-param-reassign
+    if (parametersDefault.includes(parameter)) element[parameter] = parameters[parameter]
+    // eslint-disable-next-line no-param-reassign
+    element.style[parameter] = parameters[parameter]
+  })
+
+  return element
+}
+
+const getCloseIframeVideo = () => {
+  const closeDiv = createElement('div', {
+    id             : 'closeCitedVideo',
+    height         : '35px',
+    width          : '35px',
+    position       : 'absolute',
+    bottom         : '0px',
+    right          : '0px',
+    zIndex         : '12',
+    color          : 'red',
+    transform      : 'rotate(45deg)',
+    cursor         : 'pointer',
+    transformOrigin: 'center',
+    fontSize       : '50px',
+    textAlign      : 'center',
+    lineHeight     : '35px',
+    textContent    : '+',
+  })
+
+  return closeDiv
+}
+
+const getIframeVideo = (src) => {
+  const iframe = document.createElement('iframe')
+
+  iframe.id     = 'iframeCitedVideo'
+  iframe.height = '100%'
+  iframe.width  = '100%'
+  iframe.src    = src
+
+  return iframe
+}
+
+const getContainerIframeVideo = (iframe, closeIframe) => {
+  const containerIframe = createElement('div', {
+    id          : 'containerIframeCitedVideo',
+    height      : '80%',
+    width       : '95%',
+    position    : 'absolute',
+    top         : '43%',
+    left        : '50%',
+    transform   : 'translate(-50%, -50%)',
+    zIndex      : '11',
+    display     : 'none',
+    border      : '3px solid #ffffff82',
+    borderRadius: '5px',
+    overflow    : 'hidden',
+  })
+
+  containerIframe.appendChild(iframe)
+  containerIframe.appendChild(closeIframe)
+
+  return containerIframe
+}
+
+const closeIframeVideo     = getCloseIframeVideo()
+const iframe               = getIframeVideo('')
+const containerIframeVideo = getContainerIframeVideo(iframe, closeIframeVideo)
+
+const changeUrlIframeVideo = (url) => {
+  iframe.src = url
+}
+
+const isClosedContainerIframe = () => containerIframeVideo.style.display === 'none'
+
+const closeContainerIframe = () => {
+  containerIframeVideo.style.display = 'none'
+  document.querySelector('#movie_player video').play()
+}
+
+const openContainerIframe = () => {
+  containerIframeVideo.style.display = 'block'
+}
 
 setTimeout(() => {
-  const description = document.querySelector('#content #description').textContent
-  const player      = document.querySelector('#movie_player video')
+  const containerPlayer = document.querySelector('#movie_player')
+  const player          = containerPlayer.querySelector('video')
+  const description     = document.querySelector('#content #description').textContent
+
+  containerPlayer.appendChild(containerIframeVideo)
+
+  const toggleDisplayContainerIframe = () => {
+    if (isClosedContainerIframe()) {
+      openContainerIframe()
+    } else if (!player.paused) {
+      closeContainerIframe()
+    }
+  }
+
+  closeIframeVideo.addEventListener('click', () => {
+    closeContainerIframe()
+  })
 
   const dateStartVideo = new Date()
   const getPlayerDate  = () => new Date(dateStartVideo.getTime() + player.currentTime * 1000)
@@ -21,22 +131,36 @@ setTimeout(() => {
   const matchedCited = [...description.matchAll(/\d{1,2}:\d{1,2} [-,–] https:\/\/[\w,.,/]+/g)]
     .map((x) => splitInfo(x[0], dateStartVideo))
 
-  console.log(matchedCited)
+  let lastTimeMatched = dateStartVideo
 
-  let lastTime        = formatTimeUniqueSeconds(dateStartVideo)
+  const addMatchCitedVideo = (url) => {
+    changeUrlIframeVideo(url)
+    setTimeout(() => {
+      player.pause()
+      toggleDisplayContainerIframe()
+    }, timeoutLoadVideo * 1000)
+  }
+
+  ['onplay', 'onseeking'].forEach((x) => {
+    player[x] = () => {
+      if (!isClosedContainerIframe()) closeContainerIframe()
+    }
+  })
+
   player.ontimeupdate = () => {
     const playerDate = getPlayerDate()
-    const newTime    = formatTimeUniqueSeconds(playerDate)
-    if (lastTime !== newTime) {
-      lastTime = newTime
+    if (formatTimeUniqueSeconds(lastTimeMatched) !== formatTimeUniqueSeconds((playerDate))) {
+      lastTimeMatched = playerDate
+
       const cited = matchedCited.find((x) => (
         x.date.getHours() === playerDate.getHours()
-        && x.date.getSeconds() === playerDate.getSeconds()
+        && x.date.getSeconds() - timeoutLoadVideo === playerDate.getSeconds()
         && x.date.getMinutes() === playerDate.getMinutes()
       ))
+
       if (cited) {
-        console.log(cited.url)
+        addMatchCitedVideo(`${youtubeEmbedUrl}/${cited.idVideo}`)
       }
     }
   }
-}, 500)
+}, 1500)
